fdfb09d395952772dedf17e853d52e8aab3c752c
thomas <thomasmonta98@gmail.com>
2021-06-13 17:23:15 +1000

    doen with assignment

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 /* Your Code Here */
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+  let employeeRecord = {}
+  employeeRecord.firstName = firstName
+  employeeRecord.familyName = familyName
+  employeeRecord.title = title
+  employeeRecord.payPerHour = payPerHour
+  employeeRecord.timeInEvents = []
+  employeeRecord.timeOutEvents = []
+  return employeeRecord
+}
+
+function createEmployeeRecords (array){
+  let employeeRecord = array.map(employee=>createEmployeeRecord(employee))
+  return employeeRecord
+}
+
+function createTimeInEvent (dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+  this.timeInEvents.push({
+      type: 'TimeIn',
+      hour: parseInt(hour),
+      date: date
+  })
+  return this
+}
+
+function createTimeOutEvent (dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+  this.timeOutEvents.push({
+      type: 'TimeOut',
+      hour: parseInt(hour),
+      date: date
+  })
+  return this
+}
+
+function hoursWorkedOnDate (date){
+  let timeInOnDate = this.timeInEvents.filter(employee=>employee.date === date)
+  let TimeOutOnDate = this.timeOutEvents.filter(employee=>employee.date === date)
+  return (TimeOutOnDate[0].hour - timeInOnDate[0].hour) / 100
+}
+
+function wagesEarnedOnDate (date){
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName (scrArray, firstName){
+      let matchedEmployee = scrArray.filter(employee => employee.firstName === firstName)
+      if(matchedEmployee.length === 1){
+        return matchedEmployee[0]
+      }else if(matchedEmployee.length !== 0){
+          return matchedEmployee
+      }
+      return undefined
+}
+
+function calculatePayroll (employeesRecords){
+    let arrayOfWages = employeesRecords.map(employee=>allWagesFor.call(employee))
+    return arrayOfWages.reduce((acc, curr) => acc+curr, 0)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
